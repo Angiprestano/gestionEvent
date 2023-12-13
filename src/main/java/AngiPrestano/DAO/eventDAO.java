0fcef2245a13bce7bfe1c9e8f333f7e5b900e9ea
Angela Prestano <angiprestano99@gmail.com>
2023-12-13 16:29:38 +0100

    commit metà

--- src/main/java/AngiPrestano/DAO/eventDAO.java
+++ src/main/java/AngiPrestano/DAO/eventDAO.java
@@ -1,11 +1,23 @@
 package AngiPrestano.DAO;
 
+import AngiPrestano.entities.event;
+import jdk.jfr.Event;
+
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 
 public class eventDAO {
     private final EntityManager em;
 
     public eventDAO(EntityManager em) {
         this.em = em;
+    }
+    public void save(event event) {
+        EntityTransaction transaction = em.getTransaction();
+        // inizio transazione 
+        transaction.begin();
+        em.persist(event);
+        transaction.commit();
     }
 }
