3700bbe89932b4723f6c928ac242f377e4c55b43
Angela Prestano <angiprestano99@gmail.com>
2023-12-14 16:20:18 +0100

    fine esercizio 12/12/23

--- src/main/java/AngiPrestano/Application.java
+++ src/main/java/AngiPrestano/Application.java
@@ -11,7 +11,7 @@ import javax.persistence.Persistence;
 import java.time.LocalDate;
 
 public class Application {
-    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestionEvent");
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestionevent");
 
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
@@ -19,5 +19,22 @@ public class Application {
 
         event serietv = new event("Station19", LocalDate.of(2024 , 3, 12), "Is a drama series, of fire fighters", eventType.PUBLICO, 6000);
         ed.save(serietv);
+
+        event concert = new event("Tiziano Ferro", LocalDate.of(2024, 6, 24), "The concert is in Rome", eventType.PUBLICO, 12873);
+        ed.save(concert);
+
+        // **************** LONG ID ********************* //
+
+        long id = 4;
+        event concertFromDb = ed.findById(id);
+        if (concertFromDb != null) {
+            System.out.println(concertFromDb);
+        } else {
+            System.out.println("Event is" + " " +  id + "non found");
+        }
+        ed.findByIdDelete(4);
+
+        em.close();
+        emf.close();
     }
 }
--- src/main/java/AngiPrestano/DAO/eventDAO.java
+++ src/main/java/AngiPrestano/DAO/eventDAO.java
@@ -20,4 +20,22 @@ public class eventDAO {
         em.persist(event);
         transaction.commit();
     }
+    public event findById(long id) {
+        return  em.find(event.class, id);
+    }
+
+    public void findByIdDelete(long id) {
+        event found = this.findById(id);
+
+        if(found != null) {
+            EntityTransaction transaction = em.getTransaction();
+            transaction.begin();
+            em.remove(found);
+            transaction.commit();
+            System.out.println("L'evento " + found.getTitle() + "è stato eliminato");
+        } else {
+            System.out.println("L'evento con l'id" + id + "non è stato trovato");
+        }
+    }
+
 }
